2cb4fb77fcd12c000a5e7e80beac4bf6ddd4be90
Sinh <sinhnv@omt.com.vn>
2020-02-24 23:29:09 +0700

    Tim x xa nhat

--- Tim_X_Xa_Nhat.php
+++ Tim_X_Xa_Nhat.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 24/02/2020
- * Time: 22:44:3
- */
+$float_arr = array(24,45,23,13,43,-12);
+$x= 15;
+$n = count($float_arr);
+$a = $x-$float_arr[0];
+for ($i=0; $i <= $n;$i++){
+    if (($float_arr[$i] - $x) > $a){
+        $a = $float_arr[$i];
+        $y =  $float_arr[$i];
+    }
+}
+printf("Gia tri xa gia tri x=15 nhat la: %d\n", $y);
